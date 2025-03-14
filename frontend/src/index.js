c7fb274ec7a0ef9e5c917f8e7333813cea988ca4
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-03-14 23:08:32 +0200

    added authprovider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </React.StrictMode>
   </Provider>
 );
