cf21ab4b9ac9088e590359329d3bca0b55d4d945
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-03-15 13:08:33 +0200

    added some routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,20 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import NavBar from "./pages/Home/NavBar";
 import Footer from "./pages/Home/footer";
 import Signup from "./pages/Loginsystem/Signup";
-import Contact from "./pages/Contact/contact"
+import Contact from "./pages/Contact/contact";
 import Login from "./pages/Loginsystem/Login";
 import Sidebar from "./pages/dashboard/Sidebar";
 import Setting from "./pages/dashboard/Setting";
 import Reports from "./pages/dashboard/Report";
 import Employees from "./pages/dashboard/Employees";
 import Customers from "./pages/dashboard/Customers";
-
+import Dogs from "./pages/pets/Dogs";
+import Cats from "./pages/pets/Cats";
+import Birds from "./pages/pets/Birds";
+import Bunnies from "./pages/pets/Bunnies";
+import Others from "./pages/pets/Others";
+import Adoption from "./pages/adopt/Adoption";
+import HowTo from "./pages/Home/HowTo";
 
 function App() {
   return (
@@ -26,7 +32,18 @@ function App() {
               <Footer />
             </>
           }
-        />{" "}
+        />
+        <Route
+          path="/HowTo"
+          element={
+            <>
+              <NavBar />
+              <HowTo />
+              <Footer />
+            </>
+          }
+        />
+
         <Route
           path="/Login"
           element={
@@ -43,6 +60,66 @@ function App() {
             <>
               <NavBar />
               <Signup />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/dogs"
+          element={
+            <>
+              <NavBar />
+              <Dogs />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/cats"
+          element={
+            <>
+              <NavBar />
+              <Cats />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/birds"
+          element={
+            <>
+              <NavBar />
+              <Birds />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/bunnies"
+          element={
+            <>
+              <NavBar />
+              <Bunnies />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/others"
+          element={
+            <>
+              <NavBar />
+              <Others />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/adoption"
+          element={
+            <>
+              <NavBar />
+              <Adoption />
               <Footer />
             </>
           }
@@ -113,7 +190,6 @@ function App() {
             </>
           }
         />
-
         <Route
           path="/contact"
           element={
--- frontend/src/pages/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dashboard = () => {
-  return <h2>Dashboard - Manage Pets</h2>;
-};
-
-export default Dashboard;
