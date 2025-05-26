73404f0a344eb45e6bf5ac75dfa93049030a4eea
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-05-26 06:11:08 +0300

    update for netlify frontend deployment

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ import userRoutes from "./routes/userRoutes.js";
 import analyticsRoutes from "./routes/analyticsRoutes.js";
 import settingsRoutes from "./routes/settingsRoutes.js";
 import adoptionRoutes from "./routes/adoptionRoutes.js";
+import repo
+
 // ✅ Convert __dirname for ES Module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -155,7 +157,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // ✅ Health Check Route
 app.get("/", (req, res) => {
   res.json({
-    message: `Hedj API is running on ${
+    message: `petAdoptionAgency API is running on ${
       isProduction ? "Render" : "localhost"
     } 🚀`,
   });
